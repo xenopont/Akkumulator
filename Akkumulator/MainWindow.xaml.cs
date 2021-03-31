2e74ba3d60e23ae572f132e1112b620a094689fa
Sergei Kovalenko <sergei.kovalenko@gamigo.com>
2021-03-31 23:08:05 +0200

    tray functionality

--- Akkumulator/MainWindow.xaml.cs
+++ Akkumulator/MainWindow.xaml.cs
@@ -1,4 +1,8 @@
-﻿using System.Windows;
+﻿using System;
+using System.Windows;
+using System.Windows.Forms;
+using ContextMenu = System.Windows.Controls.ContextMenu;
+using MenuItem = System.Windows.Controls.MenuItem;
 
 namespace Akkumulator
 {
@@ -7,6 +11,13 @@ namespace Akkumulator
     /// </summary>
     public partial class MainWindow : Window
     {
+        private WindowState fCurrentWindowState = WindowState.Normal;
+        public WindowState CurrentWindowState
+        {
+            get { return fCurrentWindowState; }
+            set { fCurrentWindowState = value; }
+        }
+
         public MainWindow()
         {
             InitializeComponent();
@@ -14,9 +25,136 @@ namespace Akkumulator
             Util.Ip.Start();
         }
 
+        protected override void OnSourceInitialized(EventArgs e)
+        {
+            base.OnSourceInitialized(e);
+            createTrayIcon();
+        }
+
+        private NotifyIcon TrayIcon = null;
+        private System.Windows.Controls.ContextMenu TrayMenu = null;
+
+        private bool createTrayIcon()
+        {
+            bool result = false;
+            if (TrayIcon == null)
+            { // только если мы не создали иконку ранее
+                TrayIcon = new NotifyIcon
+                {
+                    Icon = Properties.Resources.tray_icon_white, // изображение для трея
+                                                                        // обратите внимание, за ресурсом с картинкой мы лезем в свойства проекта, а не окна,
+                                                                        // поэтому нужно указать полный namespace
+                    Text = "Here is tray icon text." // текст подсказки, всплывающей над иконкой
+                }; // создаем новую
+                TrayMenu = Resources["TrayMenu"] as ContextMenu; // а здесь уже ресурсы окна и тот самый x:Key
+
+                // сразу же опишем поведение при щелчке мыши, о котором мы говорили ранее
+                // это будет просто анонимная функция, незачем выносить ее в класс окна
+                TrayIcon.Click += delegate (object sender, EventArgs e) {
+                    if ((e as System.Windows.Forms.MouseEventArgs).Button == System.Windows.Forms.MouseButtons.Left)
+                    {
+                        // по левой кнопке показываем или прячем окно
+                        ShowHideMainWindow(sender, null);
+                    }
+                    else
+                    {
+                        // по правой кнопке (и всем остальным) показываем меню
+                        TrayMenu.IsOpen = true;
+                        Activate(); // нужно отдать окну фокус, см. ниже
+                    }
+                };
+                result = true;
+            }
+            else
+            { // все переменные были созданы ранее
+                result = true;
+            }
+            TrayIcon.Visible = true; // делаем иконку видимой в трее
+            return result;
+        }
+
+        private void ShowHideMainWindow(object sender, RoutedEventArgs e)
+        {
+            TrayMenu.IsOpen = false; // спрячем менюшку, если она вдруг видима
+            if (IsVisible)
+            {// если окно видно на экране
+             // прячем его
+                Hide();
+                // меняем надпись на пункте меню
+                (TrayMenu.Items[0] as MenuItem).Header = "Show";
+            }
+            else
+            { // а если не видно
+              // показываем
+                Show();
+                // меняем надпись на пункте меню
+                (TrayMenu.Items[0] as MenuItem).Header = "Hide";
+                WindowState = CurrentWindowState;
+                Activate(); // обязательно нужно отдать фокус окну,
+                            // иначе пользователь сильно удивится, когда увидит окно
+                            // но не сможет в него ничего ввести с клавиатуры
+            }
+        }
+
+        protected override void OnStateChanged(EventArgs e)
+        {
+            base.OnStateChanged(e); // системная обработка
+            if (this.WindowState == System.Windows.WindowState.Minimized)
+            {
+                // если окно минимизировали, просто спрячем
+                Hide();
+                // и поменяем надпись на менюшке
+                (TrayMenu.Items[0] as MenuItem).Header = "Show";
+            }
+            else
+            {
+                // в противном случае запомним текущее состояние
+                CurrentWindowState = WindowState;
+            }
+        }
+
+        private bool fCanClose = false;
+        public bool CanClose
+        { // флаг, позволяющий или запрещающий выход из приложения
+            get { return fCanClose; }
+            set { fCanClose = value; }
+        }
+
+        protected override void OnClosing(System.ComponentModel.CancelEventArgs e)
+        {
+            base.OnClosing(e); // встроенная обработка
+            if (!CanClose)
+            {   // если нельзя закрывать
+                e.Cancel = true; //выставляем флаг отмены закрытия
+                // запоминаем текущее состояние окна
+                CurrentWindowState = this.WindowState;
+                // меняем надпись в менюшке
+                (TrayMenu.Items[0] as MenuItem).Header = "Show";
+                // прячем окно
+                Hide();
+            }
+            else
+            { // все-таки закрываемся
+              // убираем иконку из трея
+                TrayIcon.Visible = false;
+            }
+        }
+
+        private void MenuExitClick(object sender, RoutedEventArgs e)
+        {
+            CanClose = true;
+            Close();
+        }
+
         private void IpButtonListener(string newIp)
         {
             IpButton.Content = "My IP:  " + (newIp == Util.Ip.ERROR_DETECTING_IP ? "Unknown" : newIp);
+        }
+
+        private void IpButton_Click(object sender, RoutedEventArgs e)
+        {
+            Util.General.CopyTextToClipboard(Util.Ip.Current);
+            WindowState = WindowState.Minimized;
         }
     }
 }
