8312e86c50bb2d4902fa00cb5e11b82d468490ca
Sergei Kovalenko <sergei.kovalenko@gamigo.com>
2021-04-03 21:40:52 +0200

    functionality better split to partial declarations

---
+++ Akkumulator/MainWindow/Ip.cs
@@ -0,0 +1,20 @@
+﻿
+
+using System.Windows;
+
+namespace Akkumulator
+{
+    public partial class MainWindow
+    {
+        private void IpButtonListener(string newIp)
+        {
+            IpButton.Content = "My IP:  " + (newIp == Util.Ip.ERROR_DETECTING_IP ? "Unknown" : newIp);
+        }
+
+        private void IpButton_Click(object sender, RoutedEventArgs e)
+        {
+            Util.General.CopyTextToClipboard(Util.Ip.Current);
+            WindowState = WindowState.Minimized;
+        }
+    }
+}
